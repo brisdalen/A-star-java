80de45401ed348437f0a4b9ea7f124f83533eca9
brisdalen <bwendelborgrisdalen@gmail.com>
2021-03-22 09:08:01 +0100

    I think im decided on that I need Edges for my purpose

--- src/NavigationPoint.kt
+++ src/NavigationPoint.kt
@@ -3,13 +3,13 @@ import java.awt.Point
 class NavigationPoint(parent: NavigationPoint?, x: Int, y: Int) {
     val parent = parent
     val position: Point = Point(x, y)
+    val edges = arrayOfNulls<Edge>(8)
 
     constructor(x: Int, y: Int) : this(null, x, y)
 
     constructor(point: Point) : this(null, point.x, point.y)
 
     constructor(parent: NavigationPoint, point: Point) : this(parent, point.x, point.y)
-
 
     override fun toString(): String {
         return "[${position.x}:${position.y}]"
